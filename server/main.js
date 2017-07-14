86e5723b474bde55dfe67eeb0852333f18fac785
Enrique Correa <quiquecorreav@gmail.com>
2017-07-13 21:36:52 -0400

    added passport

--- server/main.js
+++ server/main.js
@@ -1,13 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
+import passport from "passport";
+import session from "express-session";
+import "./auth.js";
 
 require('dotenv').config();
 
 import graphQLSchema from './graphql';
 
-const PORT = 3003;
+const PORT = process.env.SERVER_PORT;
 var app = express();
+
+app.use(session({
+  secret: process.env.SESSION_SECRET
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/graphql', bodyParser.json(),
   graphqlExpress({
